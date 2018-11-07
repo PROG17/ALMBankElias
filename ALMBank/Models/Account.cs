22428cb7d3c64424f9f114fd51bd7f11cc381328
Elias Mohammed <eliasrmohammed@hotmail.com>
2018-11-07 13:53:17 +0100

    första featrure commiten en andra gång..

---
+++ ALMBank/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ALMBank.Models
+{
+    public class Account
+    {
+        public int Balance { get; set; }
+        public string AccountName { get; set; }
+        public Customer Customer{ get; set; }
+    }
+}
