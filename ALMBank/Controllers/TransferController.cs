93a4c97ac38bec70a9b3d1905a5d91a9fdb86e6e
Elias Mohammed <eliasrmohammed@hotmail.com>
2018-11-29 10:59:57 +0100

    w

--- ALMBank/Controllers/TransferController.cs
+++ ALMBank/Controllers/TransferController.cs
@@ -14,6 +14,7 @@ namespace ALMBank.Controllers
         public TransferController(BankRepository repo)
         {
             _repo = repo;
+            //kommentar 1.0
         }
 
         public IActionResult Index()
